6c40a75787486ac40e9524253fe8ca575f58edfd
xsuto <michal.sutowicz@gmail.com>
2025-04-06 21:55:40 +0200

    Remove unneeded files from self hosted version

--- app.config.ts
+++ app.config.ts
@@ -4,18 +4,6 @@ import tsConfigPaths from 'vite-tsconfig-paths'
 export default defineConfig({
   server: {
     preset: 'vercel',
-    routeRules: {
-      '/js/script.js': {
-        proxy: {
-          to: 'https://plausible.xsuto.com/js/script.js',
-        },
-      },
-      '/api/event': {
-        proxy: {
-          to: 'https://plausible.xsuto.com/api/event',
-        },
-      },
-    },
   },
   vite: {
     plugins: [
